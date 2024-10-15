2cf429904ebd72056e254cf091982949db3dd9ae
JungJay <keunhee0920@gmail.com>
2024-10-15 14:25:00 +0900

    [24.10.15] 마이바티스 엘리먼트 연습

---
+++ 01-lecture-source/chap05-mapper-element-lecture-source/src/main/java/com/ohgiraffers/common/Template.java
@@ -0,0 +1,35 @@
+package com.ohgiraffers.common;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class Template {
+
+    private static SqlSessionFactory sqlSessionFactory;
+
+    public static SqlSession getSqlSession(){
+
+        try {
+
+            if (sqlSessionFactory == null){
+
+                String resource = "config/mybatis-config.xml";
+
+                InputStream inputStream = Resources.getResourceAsStream(resource);
+
+                sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+            }
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        return sqlSessionFactory.openSession(false);
+    }
+
+}
